87a7e2e1d91b40804d8f1f6eb00e1ad8d91d91e5
rere <regis-graptin@sfr.fr>
2024-09-17 18:31:06 +0200

    feat: work on receipt

--- public_nft/src/AcknowledgeReceipt.sol
+++ public_nft/src/AcknowledgeReceipt.sol
@@ -4,28 +4,52 @@ pragma solidity ^0.8.20;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-import {IAxelarGasService} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol";
+import {AxelarExecutable} from "axelar-gmp-sdk-solidity/executable/AxelarExecutable.sol";
+import {IAxelarGasService} from "axelar-gmp-sdk-solidity/interfaces/IAxelarGasService.sol";
+import {StringToAddress, AddressToString} from "axelar-gmp-sdk-solidity/libs/AddressString.sol";
 
-contract AcknowledgeReceipt is ERC721URIStorage {
+contract AcknowledgeReceipt is ERC721URIStorage, AxelarExecutable {
+    using StringToAddress for string;
+    using AddressToString for address;
+
     uint256 private _nextTokenId;
 
     // Recipient of the given NFT
-    mapping (uint256 => address) recipient;
+    mapping(uint256 => address) recipient;
 
     // Indicate if the recipient has reveal the NFT content
-    mapping (uint256 => address) reveal;
+    mapping(uint256 => address) reveal;
 
-    event RevealEvent (uint256 indexed tokenId, address indexed recipient);
+    IAxelarGasService public immutable gasService;
+    string public chainName; // FIXME :: name of the chain this contract is deployed to
 
-    constructor() ERC721("AcknowledgeReceipt", "ART") {}
+    string secretSmartContractAddress;
 
-    function createReceipt(
-        address player, 
-        string memory tokenURI
-    ) public returns (uint256) {
+    event RevealEvent(uint256 indexed tokenId, address indexed recipient);
+
+    constructor(address gateway_, address gasReceiver_, string memory chainName_)
+        ERC721("AcknowledgeReceipt", "ART")
+        AxelarExecutable(gateway_)
+    {
+        gasService = IAxelarGasService(gasReceiver_);
+        chainName = chainName_;
+    }
+
+    function createReceipt(address _recipient, string memory tokenURI, string calldata encryptedMessage)
+        public
+        payable
+        returns (uint256)
+    {
+        // Mint a new NFT
         uint256 tokenId = _nextTokenId++;
-        _mint(player, tokenId);
+        _mint(_recipient, tokenId);
         _setTokenURI(tokenId, tokenURI);
+
+        recipient[tokenId] = _recipient;
+
+        // Store the encrypted part
+        _send_axelar_msg(encryptedMessage);
+
         return tokenId;
     }
 
@@ -34,6 +58,50 @@ contract AcknowledgeReceipt is ERC721URIStorage {
 
         reveal[tokenId] = msg.sender;
 
+        // _send_axelar_msg("reveal"); // FIXME :: Need to send an action data for the given tokenId and address
+
         emit RevealEvent(tokenId, msg.sender);
+    }
+
+    function _send_axelar_msg(string calldata message) internal {
+        // 1. Generate GMP payload
+        bytes memory executeMsgPayload = abi.encode(message);
+        bytes memory payload = _encodePayloadToCosmWasm(executeMsgPayload);
+
+        // 2. Pay for gas
+        gasService.payNativeGasForContractCall{value: msg.value}(
+            address(this), "secret", secretSmartContractAddress, payload, msg.sender
+        );
+
+        // 3. Make GMP call
+        gateway.callContract("secret", secretSmartContractAddress, payload);
+    }
+
+    function _encodePayloadToCosmWasm(bytes memory executeMsgPayload) internal view returns (bytes memory) {
+        // Schema
+        //   bytes4  version number (0x00000001)
+        //   bytes   ABI-encoded payload, indicating function name and arguments:
+        //     string                   CosmWasm contract method name
+        //     dynamic array of string  CosmWasm contract argument name array
+        //     dynamic array of string  argument abi type array
+        //     bytes                    abi encoded argument values
+
+        // contract call arguments for ExecuteMsg::receive_message_evm{ source_chain, source_address, payload }
+        bytes memory argValues = abi.encode(chainName, address(this).toString(), executeMsgPayload);
+
+        string[] memory argumentNameArray = new string[](3);
+        argumentNameArray[0] = "source_chain";
+        argumentNameArray[1] = "source_address";
+        argumentNameArray[2] = "payload";
+
+        string[] memory abiTypeArray = new string[](3);
+        abiTypeArray[0] = "string";
+        abiTypeArray[1] = "string";
+        abiTypeArray[2] = "bytes";
+
+        bytes memory gmpPayload;
+        gmpPayload = abi.encode("receive_message_evm", argumentNameArray, abiTypeArray, argValues);
+
+        return abi.encodePacked(bytes4(0x00000001), gmpPayload);
     }
 }
